73895086943f0df186de1d8ca63e0d63f4f2ff21
Hovhannes Stepanyan <hovhannesstepanyan@Hovhanness-Mac-mini.local>
2018-06-25 15:40:33 +0400

    database get request

---
+++ LocalizationBackend/src/main/kotlin/Constants.kt
@@ -0,0 +1,3 @@
+enum class Constants(type: String) {
+    CREATE("create")
+}
--- LocalizationBackend/src/main/kotlin/Main.kt
+++ LocalizationBackend/src/main/kotlin/Main.kt
@@ -6,7 +6,11 @@ import org.jetbrains.ktor.routing.routing
 import com.google.gson.Gson
 import javafx.scene.web.WebHistory
 import kotlinx.coroutines.experimental.newCoroutineContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.jetbrains.ktor.application.log
+import org.jetbrains.ktor.application.receive
+import org.jetbrains.ktor.client.RequestBuilder
 import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.http.ContentType
 import org.jetbrains.ktor.http.HttpMethod
@@ -17,22 +21,31 @@ import org.jetbrains.ktor.routing.post
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080) {
         routing {
-            get("/api") {
-                var headers = call.request.httpMethod == HttpMethod.Get
-                var gson = Gson()
-                var str = gson.toJson(headers)
+            get("/api/database") {
+                val queries = call.request.queryParameters
+                val dbName = queries["name"]
+                val client = OkHttpClient()
+                val request = Request.Builder().url("https://localization-1be56.firebaseio.com/" + dbName + ".json")
+                        .build()
+                val response = client.newCall(request).execute()
+                val str = response.body()?.string()
                 call.response.header("Access-Control-Allow-Origin", "*")
-                call.respondText(str, ContentType.Application.Json)
+                call.respondText(str.toString(), ContentType.Application.Json)
+            }
+            post("/api/{path}/") {
+                val body = call.request.receive<String>()
+                val path = call.parameters["path"]
+                if (path.equals(Constants.CREATE.toString(), true)) {
+                    val json = Gson().fromJson(body, HashMap<String, String>().javaClass)
+                    var appAlias = json["app_alias"]
+
+                    call.respondText(appAlias.toString())
+                } else {
+                    call.respond("Unknown path")
+                }
             }
         }
-    }
-    embeddedServer(Netty, 8080) {
-        routing {
-            post("/api/create") {
-                var body = call.request
-            }
-        }
-    }
+    }.start(true)
 }
 
 data class Entry(val message: String)
