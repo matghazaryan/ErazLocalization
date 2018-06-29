dc0229cd5b0d259a41ec9f6b0613d393b0eadb68
Grigor Hakobyan <>
2018-06-29 18:54:28 +0400

    Show project localization

--- LocalizationFrontend/src/main/kotlin/main.kt
+++ LocalizationFrontend/src/main/kotlin/main.kt
@@ -1,12 +1,12 @@
 import org.w3c.dom.*
 import org.w3c.dom.events.Event
-import org.w3c.xhr.XMLHttpRequest
 import kotlin.browser.document
 import kotlin.browser.window
 import kotlin.js.Json
 
 
-external fun require(module:String):dynamic
+
+external fun require(module:String): dynamic
 
 // Բայց սոռտ է, մարդավարի ES5/ES6 ով կգրես, require('firebase/app') կամ import firebase from 'firebase'
 // չի աշխատի, տողի վրա կգրես, կաշխատի 😂
@@ -59,7 +59,7 @@ fun main(args: Array<String>) {
                         val cardElement = element.firstElementChild
                         if (cardElement != null) {
                             val projectAlias = cardElement.getAttribute("data-alias")
-                            window.location.href = "file:///Users/grigorhakobyan/IdeaProjects/ErazLocalization/LocalizationFrontend/src/main/resources/project.html?alias=${projectAlias}"
+                            window.location.href = "./project.html?alias=${projectAlias}"
                         }
                     })
                 }
@@ -72,85 +72,110 @@ fun main(args: Array<String>) {
             val collectionElement = document.getElementById("collection-header")
 
             getProject(targetProjectAlias) {
-                val projectName = it["name"]
-                val projectAlias = it["alias"]
+
+                val projectName = it["name"] as String
+                val projectAlias = it["alias"] as String
+
+                var languages = arrayListOf<String>()
+                val languagesJson = it["languages"] as Json
+                js("Object").values(languagesJson).forEach(fun (language: dynamic) {
+                    languages.add(language["langName"] as String)
+                })
 
                 if (collectionElement != null) {
 
-                    var innerHtml =  "<div class=\"collection-header\">\n" +
-                            "             <h5>${projectName}</h5>\n" +
-                            "             <h6>${projectAlias}</h6>\n" +
-                            "         </div>"
-                    
-                    innerHtml += "<table>\n" +
-                            "        <thead>\n" +
-                            "          <tr>\n" +
-                            "              <th>N</th>\n" +
-                            "              <th>Screen</th>\n" +
-                            "              <th>Type</th>\n" +
-                            "              <th>Key</th>\n" +
-                            "              <th>Type</th>\n" +
-                            "          </tr>\n" +
-                            "        </thead>\n" +
-                            "\n" +
-                            "        <tbody>\n" +
-                            "          <tr>\n" +
-                            "            <td>Alvin</td>\n" +
-                            "            <td>Eclair</td>\n" +
-                            "            <td>\$0.87</td>\n" +
-                            "          </tr>\n" +
-                            "          <tr>\n" +
-                            "            <td>Alan</td>\n" +
-                            "            <td>Jellybean</td>\n" +
-                            "            <td>\$3.76</td>\n" +
-                            "          </tr>\n" +
-                            "          <tr>\n" +
-                            "            <td>Jonathan</td>\n" +
-                            "            <td>Lollipop</td>\n" +
-                            "            <td>\$7.00</td>\n" +
-                            "          </tr>\n" +
-                            "        </tbody>\n" +
-                            "      </table>"
+                    var innerHtml =
+                            "<div class=\"collection-header\">" +
+                            "<h5>${projectName}</h5>" +
+                            "<h6>${projectAlias}</h6>" +
+                                    "<dic>" +
+                                    "  <!-- Dropdown Trigger -->\n" +
+                                    "  <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>\n" +
+                                    "\n" +
+                                    "  <!-- Dropdown Structure -->\n" +
+                                    "  <ul id='dropdown1' class='dropdown-content'>\n" +
+                                    "    <li><a href=\"#!\">one</a></li>\n" +
+                                    "    <li><a href=\"#!\">two</a></li>\n" +
+                                    "    <li class=\"divider\" tabindex=\"-1\"></li>\n" +
+                                    "    <li><a href=\"#!\">three</a></li>\n" +
+                                    "    <li><a href=\"#!\"><i class=\"material-icons\">view_module</i>four</a></li>\n" +
+                                    "    <li><a href=\"#!\"><i class=\"material-icons\">cloud</i>five</a></li>\n" +
+                                    "  </ul>" +
+                                    "</div>" +
+                            "</div>"
 
+                    val tableData =
+                            "<table class=\"highlight centered responsive-table\">" +
+                            "<thead>" +
+                            "<tr>" +
+                            getColumNames(languages) +
+                            "</tr>" +
+                            "</thead>" +
+                            "<tbody>" +
+                            getRows(it) +
+                            "</tbody>" +
+                            "</table>"
 
-
+                    innerHtml += tableData
                     collectionElement.innerHTML = innerHtml
                 }
-
-
-
-
-
-
-
-
-
-                console.log(it)
             }
         }
     }
 }
 
-/// Helpers
-/*
-fun loadJSON(callBack: (HashMap<String, String>) -> Unit) {
-    /*val req = XMLHttpRequest()
-    req.overrideMimeType("application/json")
-    req.open("GET", "./langs.json", true)
-    req.onreadystatechange = {
-        if (req.readyState == 4.toShort() && req.status == 200.toShort()) {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            val map = JSON.parse<HashMap<String, String>>(req.responseText)
-            callBack(map)
+
+
+fun getColumNames(languages: ArrayList<String>): String {
+    var str =  "<th>N</th>" + "<th>Screen</th>" +  "<th>Key</th>"
+    for (language in languages) {
+        str += "<th>${language}</th>"
+    }
+    return  str
+}
+
+fun getRows(json: Json): String {
+    var str = ""
+    var index = 0
+
+    var screens = arrayListOf<String>()
+    val screensJson = json["screens"] as Json
+    js("Object").values(screensJson).forEach(fun (screen: String) {
+        screens.add(screen)
+    })
+
+    val localization = json["localization"] as Json
+    for (screen in screens) {
+
+        val screenLocalization = localization[screen] as? Json
+        if (screenLocalization != null) {
+
+
+            js("Object").values(screenLocalization).forEach(fun (localization: dynamic) {
+                index++
+                val key = localization["key"] as String
+
+                str +=  "<tr>" +
+                        "<td>${index}</td>" +
+                        "<td>${screen}</td>" +
+                        "<td>${key}</td>"
+
+                var values = arrayListOf<String>()
+                val valuesJson = localization["values"] as Json
+                js("Object").values(valuesJson).forEach(fun (value: dynamic) {
+                    val languageKey = value["lang_key"] as String
+                    val languageValue = value["lang_value"] as String
+                    str += "<td>${languageValue}</td>"
+                })
+
+                str += "</tr>"
+            })
+
+        } else {
+            continue
         }
     }
-    req.send()*/
-    val json = js("langs")
-    val map = hashMapOf<String, String>()
-    js("Object").keys(json).forEach(fun (key: String) {
-        map.put(key, json[key])
-    })
-    callBack(map)
+
+    return str
 }
-*/
 
