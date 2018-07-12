7849c76d90364d73e8a6469ba840563540603219
hovo-infinity <stepanyan.hovo.95@gmail.com>
2018-07-12 15:47:14 +0400

    search by screenName

--- LocalizationFrontend/src/main/kotlin/main.kt
+++ LocalizationFrontend/src/main/kotlin/main.kt
@@ -303,9 +303,78 @@ fun main(args: Array<String>) {
                             option.value = it
                             option.text = it
                             select.appendChild(option)
-                            console.log(it)
                         }
+                        val label = document.createElement("label") as HTMLLabelElement
+                        label.innerText = "Filter by screen name"
+                        comboBox.append(select, label)
+                        headerContainerBase.appendChild(comboBox)
+
+                        val searchField = document.createElement("div")
+                        searchField.addClass("input-field")
+                        searchField.addClass("col")
+                        searchField.addClass("s6")
+                        val icon = document.createElement("i")
+                        icon.addClass("material-icons")
+                        icon.addClass("prefix")
+                        icon.innerHTML = "search"
+                        val input = document.createElement("input") as HTMLInputElement
+                        input.addClass("validate")
+                        input.id = "icon_search"
+                        val placeholder = document.createElement("label") as HTMLLabelElement
+                        placeholder.htmlFor = "icon_search"
+                        placeholder.innerText = "Search"
+                        searchField.append(icon, input, placeholder)
+                        headerContainerBase.appendChild(searchField)
+                        input.addEventListener("input", fun (e: Event) {
+                            select.selectedIndex = 2
+                            val searchText = input.value
+                            tableBody.innerHTML = ""
+                            index = 0
+                            if (searchText.isEmpty()) {
+                                if (localization != null) {
+                                    Object().values(it["screens"]).forEach(fun(screen: String) {
+                                        val screenLocalization = localization[screen] as? Json
+                                        tableRowDataFromScreen(screen, screenLocalization).forEach {
+                                            index++
+                                            val tr = tableRowElementFromTableRowData(it, index)
+                                            tableBody.appendChild(tr)
+                                        }
+                                    })
+                                }
+                            } else {
+                                // first Object().values(localization) is an array of screens localizations
+                                // val screen is the values of screens localizations
+                                val localizationsOfscreens = arrayOf<Json>()
+                                Object().keys(localization).forEach(fun (key: String) {
+                                    val elem = localization?.get(key) as Json
+                                    Object().values(elem).forEach(fun (el :Json) {
+                                        localizationsOfscreens.set(localizationsOfscreens.count(), json(key to el))
+                                    })
+                                })
+                                val predicate: ((Json)) -> Boolean = {
+                                    var contains = false
+                                    val values = Object().values(it)[0]["values"] as Json
+                                    Object().values(values).forEach(fun (el: Json) {
+                                        val x = el["lang_value"].toString().contains(searchText, true)
+                                        if (x) {
+                                            contains = x
+                                        }
+                                    })
+                                    contains
+                                }
+                                val filteredScreens = localizationsOfscreens.filter(predicate)
+                                filteredScreens.forEach {
+                                    val screenName = Object().keys(it)[0].toString()
+                                    tableRowDataFromScreen(screenName, it).forEach {
+                                        index++
+                                        val tr = tableRowElementFromTableRowData(it, index)
+                                        tableBody.appendChild(tr)
+                                    }
+                                }
+                            }
+                        })
                         select.onchange = { _ ->
+                            input.value = ""
                             tableBody.innerHTML = ""
                             index = 0
                             if ((select.selectedOptions[0] as HTMLOptionElement).value != "all") {
@@ -329,62 +398,6 @@ fun main(args: Array<String>) {
                                 }
                             }
                         }
-                        val label = document.createElement("label") as HTMLLabelElement
-                        label.innerText = "Filter by screen name"
-                        comboBox.append(select, label)
-                        headerContainerBase.appendChild(comboBox)
-
-                        val searchField = document.createElement("div")
-                        searchField.addClass("input-field")
-                        searchField.addClass("col")
-                        searchField.addClass("s6")
-                        val icon = document.createElement("i")
-                        icon.addClass("material-icons")
-                        icon.addClass("prefix")
-                        icon.innerHTML = "search"
-                        val input = document.createElement("input") as HTMLInputElement
-                        input.addClass("validate")
-                        input.id = "icon_search"
-                        val placeholder = document.createElement("label") as HTMLLabelElement
-                        placeholder.htmlFor = "icon_search"
-                        placeholder.innerText = "Search"
-                        searchField.append(icon, input, placeholder)
-                        input.addEventListener("input", fun (e: Event) {
-                            val project = it
-                            val searchText = input.value
-                            tableBody.innerHTML = ""
-                            index = 0
-                            if (searchText.isEmpty()) {
-                                if (localization != null) {
-                                    Object().values(it["screens"]).forEach(fun(screen: String) {
-                                        val screenLocalization = localization[screen] as? Json
-                                        tableRowDataFromScreen(screen, screenLocalization).forEach {
-                                            index++
-                                            val tr = tableRowElementFromTableRowData(it, index)
-                                            tableBody.appendChild(tr)
-                                        }
-                                    })
-                                }
-                            } else {
-
-                            }
-                            if (localization != null) {
-
-                                if ((select.selectedOptions[0] as HTMLOptionElement).value != "all") {
-                                    filterScreens(projectName, (select.selectedOptions[0] as HTMLOptionElement).value, fun(screen: Json) {
-                                        tableRowDataFromScreen((select.selectedOptions[0] as HTMLOptionElement).value, screen).forEach {
-                                            index++
-                                            val tr = tableRowElementFromTableRowData(it, index)
-                                            tableBody.appendChild(tr)
-                                        }
-                                    })
-                                } else {
-
-                                }
-                            }
-
-                        })
-                        headerContainerBase.appendChild(searchField)
                     }
 
                     val floatButton = document.createElement("div") as HTMLDivElement
